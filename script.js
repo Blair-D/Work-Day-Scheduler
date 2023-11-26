610a2a9a552b4c16a9d7563f953bfea508f2293d
B Dowling <Blair0789@gmail.com>
2023-11-26 13:39:01 -0500

    debugged broken code for local storage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.saveBtn').on('click', function () {
     var time = $(this).parent().attr('id');
     var value = $(this).siblings('.description').val();
-    localStorage.setItem(value, time);
+    localStorage.setItem(time, value);
   });
     
   function hourUpdater() {
