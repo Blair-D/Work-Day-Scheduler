3b7955b2d3acf5b2f8332bbeb517e885e462f4c5
B Dowling <Blair0789@gmail.com>
2023-11-26 12:51:36 -0500

    added javascript code

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+$(document).ready(function () {
+  $('.saveBtn').on('click', function () {
+    var time = $(this).parent().attr('id');
+    var value = $(this).siblings('.description').val();
+    localStorage.setItem(value, time);
+  });
+    
+  function hourUpdater() {
+    var currentHour = dayjs().hour();
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+
+  hourUpdater();
+  setInterval(hourUpdater, 10000);
+
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+  $('#currentDay').text(dayjs().format('MMMM D, YYYY : dddd '));
+});
 
